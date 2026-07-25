7d55a8dc7674c01fed64483d85d549fb7535c39d
Abhishek <abhishekkanichery@gmail.com>
2026-07-25 12:28:39 +0530

    feat: embed user query from chat and did similarity search from vector store

--- apps/api/src/chat/chat.service.ts
+++ apps/api/src/chat/chat.service.ts
@@ -11,11 +11,20 @@ class ChatService {
   chat = async (dto: UserChatQueryType) => {
     // Save message
     // Retrieve history
+
     // Embed user query.
+    const embeddedQuery = await this.embeddingProvider.embedChunk(
+      dto.userQuery,
+    );
 
     // Retrive rag context
+    const memoryContext = await this.vectorStoreProvider.similaritySearch(
+      embeddedQuery,
+      5,
+    );
 
     // Call LLM with all these context
+
     // Receive chunks
     // Forward chunks immediately
     // Append chunks into answer buffer
