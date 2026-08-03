44ff6bffc1efe56dc65d936ec56aa32bed351ad6
Abhishek <abhishekkanichery@gmail.com>
2026-08-03 22:01:59 +0530

    feat: user-api-tokens

--- packages/database/src/schema/agents/relations.ts
+++ packages/database/src/schema/agents/relations.ts
@@ -1,11 +1,13 @@
 import { defineRelations } from "drizzle-orm";
 import { usersTable } from "../auth";
 import { agentsTable } from "./agents";
+import { userApiKeysTable } from "./user-api-keys";
 
 export const agentsRelations = defineRelations(
   {
     users: usersTable,
     agents: agentsTable,
+    userApiTokens: userApiKeysTable,
   },
   (relations) => ({
     agents: {
@@ -14,8 +16,15 @@ export const agentsRelations = defineRelations(
         to: relations.users.id,
       }),
     },
+    userApiTokens: {
+      owner: relations.one.users({
+        from: relations.userApiTokens.userId,
+        to: relations.users.id,
+      }),
+    },
     users: {
       agents: relations.many.agents(),
+      userApiTokens: relations.many.userApiTokens(),
     },
   }),
 );
--- packages/database/src/schema/agents/user-api-keys.ts
+++ packages/database/src/schema/agents/user-api-keys.ts
@@ -1,10 +1,15 @@
 import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 import { auditColumns } from "../common/audit";
 import { providerEnum } from "../common/enum";
+import { usersTable } from "../auth";
 
 export const userApiKeysTable = pgTable("user_api_keys", {
   id: uuid("id").primaryKey().notNull().defaultRandom(),
-  userId: uuid("user_id").notNull(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => usersTable.id, {
+      onDelete: "cascade",
+    }),
   provider: providerEnum("provider").default("openai"),
   encryptedKey: varchar("encrypted_key").notNull(),
   ...auditColumns,
