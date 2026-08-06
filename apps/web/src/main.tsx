d4899a7e5c68a0c810bba25ef92eaac0373bd71b
Abhishek <abhishekkanichery@gmail.com>
2026-08-06 08:28:18 +0530

    feat: added lucid icons and react router dom

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,9 +1,11 @@
 import { createRoot } from "react-dom/client";
 import "./style.css";
+import { Button } from "@/components/ui/button";
 
 const App = () => (
   <div>
     <h1>Retriev</h1>
+    <Button>Button</Button>
   </div>
 );
 
