8ce28ca514d11aee9d8aa5c66ce22f0a47cb10d5
Abhishek <abhishekkanichery@gmail.com>
2026-08-08 22:25:25 +0530

    refc: naming convention

--- apps/web/src/features/auth/pages/login-page.tsx
+++ apps/web/src/features/auth/pages/login-page.tsx
@@ -1,6 +1,8 @@
 import LoginCard from "../components/LoginCard";
+import { authClient } from "@/lib/auth-client";
 
 export const LoginPage = () => {
+  console.log(authClient);
   return (
     <div>
       <LoginCard />
