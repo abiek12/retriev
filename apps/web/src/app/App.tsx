2f9bb9429c1c48cb6b8f38ac2fd4ba117cd85b45
Abhishek <abhishekkanichery@gmail.com>
2026-08-09 12:25:40 +0530

    feat(web): configured query globally using provider

--- apps/web/src/app/App.tsx
+++ apps/web/src/app/App.tsx
@@ -1,8 +1,21 @@
+import { QueryProvider } from "@/components/providers/QueryProvider";
+import ThemeProvider from "@/components/providers/ThemeProvider";
 import { routes } from "@/routes";
 import { RouterProvider } from "react-router-dom";
 
 const App = () => {
-  return <RouterProvider router={routes} />;
+  return (
+    <QueryProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <RouterProvider router={routes} />
+      </ThemeProvider>
+    </QueryProvider>
+  );
 };
 
 export default App;
--- apps/web/src/app/main.tsx
+++ apps/web/src/app/main.tsx
@@ -1,14 +1,11 @@
 import { createRoot } from "react-dom/client";
 import "@/styles/style.css";
-import Providers from "./Providers";
 import { StrictMode } from "react";
 import App from "./App";
 
 const Root = () => (
   <StrictMode>
-    <Providers>
-      <App />
-    </Providers>
+    <App />
   </StrictMode>
 );
 
