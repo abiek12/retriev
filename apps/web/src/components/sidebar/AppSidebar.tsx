98d4207033637041fe7ed42d1bff8bffa6aabdf2
Abhishek <abhishekkanichery@gmail.com>
2026-09-03 23:45:32 +0530

    enhc: update logo

--- apps/web/src/components/sidebar/AppSidebar.tsx
+++ apps/web/src/components/sidebar/AppSidebar.tsx
@@ -38,10 +38,53 @@ const navigation = [
 
 export const AppSidebar = () => {
   return (
-    <div className="w-xs h-full flex flex-col bg-sidebar p-6">
-      <div className="header flex items-center justify-start gap-3 w-full py-3">
-        <div className="logo">R</div>
-        <h1>Retriev</h1>
+    <div className="w-xs h-full flex flex-col bg-sidebar p-6 select-none">
+      <div className="header flex items-center justify-start gap-2 w-full my-2">
+        <div className="logo cursor-pointer">
+          <svg
+            viewBox="0 0 120 120"
+            width="40"
+            height="40"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <defs>
+              <mask id="slit-cutout-black">
+                <rect width="120" height="120" fill="white" />
+                <path
+                  d="M -10 60 L 46 60 A 14 14 0 0 1 60 74 L 60 130"
+                  stroke="black"
+                  stroke-width="7"
+                  fill="none"
+                  stroke-linecap="butt"
+                />
+              </mask>
+            </defs>
+
+            <g fill="#000000" mask="url(#slit-cutout-black)">
+              <rect x="51" y="10" width="14" height="100" rx="1" />
+              <rect x="10" y="51" width="100" height="14" rx="1" />
+              <rect
+                x="51"
+                y="10"
+                width="14"
+                height="100"
+                rx="1"
+                transform="rotate(45 60 60)"
+              />
+              <rect
+                x="51"
+                y="10"
+                width="14"
+                height="100"
+                rx="1"
+                transform="rotate(-45 60 60)"
+              />
+            </g>
+          </svg>
+        </div>
+        <h1 className="text-3xl font-semibold tracking-tight font-heading">
+          Retriev
+        </h1>
       </div>
 
       <nav className="my-6 flex flex-col gap-1">
